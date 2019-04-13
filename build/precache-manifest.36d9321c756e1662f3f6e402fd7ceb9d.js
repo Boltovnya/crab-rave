2da64e17c728c1f6a19ef6e7047b1a0685d756b7
Erin Murphy <boltovnya@Erins-MBP.connect>
2019-04-14 00:21:44 +0100

    updated server.js

---
+++ build/precache-manifest.36d9321c756e1662f3f6e402fd7ceb9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f5f2db868cb8f87b223c",
+    "url": "/static/js/main.2c17a7e1.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "f5f2db868cb8f87b223c",
+    "url": "/static/css/main.3bcd7f31.chunk.css"
+  },
+  {
+    "revision": "917010871d4ed427cf5cc9e8183efa0e",
+    "url": "/index.html"
+  }
+];
